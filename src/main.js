5716623f4c3b25fb036d2e31000de223237ae35d
Iggy <igoririanto@rocketmail.com>
2019-05-13 17:23:35 -0500

    working / rendering per second

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import render from "./vdom/render";
 import createElement from "./vdom/createElement";
 import mount from "./vdom/mount";
 
+/* returns object necessary to createElement */
 const createVApp = count =>
   createElement("div", {
     attrs: {
@@ -11,6 +12,7 @@ const createVApp = count =>
     },
     children: [
       String(count),
+      createElement("input"), // gets refreshed per second! A problem.
       createElement("img", {
         attrs: {
           src: "https://media.giphy.com/media/26gsf19j60Wh8GlcQ/giphy.gif"
@@ -20,12 +22,14 @@ const createVApp = count =>
   });
 
 let count = 0;
-const vApp = createVApp(count);
-const $app = render(vApp);
+const vApp = createVApp(count); // vApp is createVApp with count var
 
-let $rootEl = mount($app, document.getElementById("app"));
+const $app = render(vApp); // $app is the actual HTML element, ready to replaceWith $rootEl
+
+let $rootEl = mount($app, document.getElementById("app")); // rootElement is where we will attach our HTML elements
+// mount(what, toWhere)
 
 setInterval(() => {
-  count++;
-  $rootEl = mount(render(createVApp(count)), $rootEl);
+  count++; // each second, increment by 1000
+  $rootEl = mount(render(createVApp(count)), $rootEl); // mounts to id="app" element, the entire HTML element from render(createVApp(count)) - this one will have incrementally different count
 }, 1000);
